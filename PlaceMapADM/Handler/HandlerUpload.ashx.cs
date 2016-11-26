a52881ecdd8f504ee9ee41c27efc8278d21c24b7
chinhnb <bachinh2105@gmail.com>
2016-11-26 09:32:49 +0700

    4

---
+++ PlaceMapADM/Handler/HandlerUpload.ashx.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+
+namespace PlaceMapADM.Handler
+{
+    /// <summary>
+    /// Summary description for HandlerUpload
+    /// </summary>
+    public class HandlerUpload : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {                        
+            try
+            {
+                var folder = "/Upload/";
+                if (context.Request.Params["folder"] != null && !string.IsNullOrEmpty(context.Request.Params["folder"]))
+                {
+                    folder = folder + context.Request.Params["folder"] + "/";
+                }
+                string dirFullPath = HttpContext.Current.Server.MapPath(folder);
+                string[] files;
+                int numFiles;
+                files = Directory.GetFiles(dirFullPath);
+                numFiles = files.Length;
+                numFiles = numFiles + 1;
+                string fileName = "";
+
+                foreach (string s in context.Request.Files)
+                {
+                    HttpPostedFile file = context.Request.Files[s];
+                    fileName = file.FileName;
+                    string fileExtension = file.ContentType;
+
+                    if (!string.IsNullOrEmpty(fileName))
+                    {
+                        //fileExtension = Path.GetExtension(fileName);
+                        //fileName = "MyPHOTO_" + numFiles.ToString() + fileExtension;
+                        string pathToSave = dirFullPath + fileName;
+                        file.SaveAs(pathToSave);
+                    }
+                }
+                context.Response.ContentType = "text/plain";
+                context.Response.Write(folder + fileName);
+            }
+            catch (Exception ex)
+            {
+                context.Response.Write("error");
+            }
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
