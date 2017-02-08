5c827cdf09d09589ad8275ecd9d88d3ab149d7d1
chinhnb <bachinh2105@gmail.com>
2017-02-08 23:18:09 +0700

    1

--- PlaceMapADM/Handler/HandlerUpload.ashx.cs
+++ PlaceMapADM/Handler/HandlerUpload.ashx.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Web;
 using System.IO;
@@ -39,7 +42,12 @@ namespace PlaceMapADM.Handler
                             var fileExtension = Path.GetExtension(fileName);
                             fileName = fileName.Replace(fileExtension,"") + "_" + numFiles + fileExtension;
                             var pathToSave = dirFullPath + fileName;
-                            file.SaveAs(pathToSave);
+                            //file.SaveAs(pathToSave); //anh goc
+
+                            //rezize 600x600
+                            Image bm = Image.FromStream(file.InputStream);
+                            bm = RezizeImage(bm, 600, 600);
+                            bm.Save(pathToSave);
                         }
                     }
                 }
@@ -58,6 +66,34 @@ namespace PlaceMapADM.Handler
             {
                 return false;
             }
+        }
+
+        private Image RezizeImage(Image img, int maxWidth, int maxHeight)
+        {
+            if (img.Height < maxHeight && img.Width < maxWidth) return img;
+            using (img)
+            {
+                Double xRatio = (double)img.Width / maxWidth;
+                Double yRatio = (double)img.Height / maxHeight;
+                Double ratio = Math.Max(xRatio, yRatio);
+                int nnx = (int)Math.Floor(img.Width / ratio);
+                int nny = (int)Math.Floor(img.Height / ratio);
+                Bitmap cpy = new Bitmap(nnx, nny, PixelFormat.Format32bppArgb);
+                using (Graphics gr = Graphics.FromImage(cpy))
+                {
+                    gr.Clear(Color.Transparent);
+
+                    // This is said to give best quality when resizing images
+                    gr.InterpolationMode = InterpolationMode.HighQualityBicubic;
+
+                    gr.DrawImage(img,
+                        new Rectangle(0, 0, nnx, nny),
+                        new Rectangle(0, 0, img.Width, img.Height),
+                        GraphicsUnit.Pixel);
+                }
+                return cpy;
+            }
+
         }
     }
 }
