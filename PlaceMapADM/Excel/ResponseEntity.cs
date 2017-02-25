77acfed04a25b63630b739a0f153b8f1b69ac2c3
chinhnb <bachinh2105@gmail.com>
2017-02-26 01:21:59 +0700

    1

---
+++ PlaceMapADM/Excel/ResponseEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PlaceMapADM.Excel
+{
+    public class ResponseEntity
+    {
+        public bool Status { get; set; }
+        public string Data { get; set; }
+    }
+}
